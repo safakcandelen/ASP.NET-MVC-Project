0b90d8e2eaf7a7f5b1416cf9bf9ffe1a21729f00
safakcandelen <safakcandelen@gmail.com>
2023-03-08 23:39:24 +0300

    Travel blog site designed using ASP.NET MVC
    Here is the my first ASP.NET MVC project

---
+++ TatilBlogSitesi/TatilBlogSitesi/Controllers/İletisimController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TatilBlogSitesi.Controllers
+{
+    public class İletisimController : Controller
+    {
+        // GET: İletisim
+        
+        public ActionResult Index()
+        {
+            
+
+             return View();
+        }
+        
+    }
+}
---
+++ TatilBlogSitesi/TatilBlogSitesi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TatilBlogSitesi.MvcApplication" Language="C#" %>
